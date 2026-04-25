0ba5c756f2d26a31e749443986b21544f0f3622a
AKescobar <akescoba@asu.edu>
2026-04-25 00:55:43 -0500

    Adding JQuery

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 "use strict";
+
+/*Songs*/
+let $slideshow = $("#slideshow");
+
+$($slideshow).on("click", function(){
+    if ($slideshow.is(".cycle-paused"))
+      $slideshow.cycle("resume");                
+   else
+      $slideshow.cycle("pause");
+});
+
+$("#previous-btn").on("click", function() {
+   $slideshow.cycle("stop");
+   $slideshow.cycle("prev");
+});
+
+$("#next-btn").on("click", function() {
+   $slideshow.cycle("stop");
+   $slideshow.cycle("next");
+});
+
+/*Softwares - Mock Server*/
+
+/*Form Validation*/
+
+/*Accordion*/
